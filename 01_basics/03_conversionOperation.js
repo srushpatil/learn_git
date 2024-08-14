806f663a505a73663f3bab7bd1700a402269de4e
srushpatil <srushpatil231102@gmail.com>
2024-08-14 05:25:04 +0000

    conversionOperations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -12,3 +12,15 @@ console.log(valueInNumber);  // gives NAN as a output
 //"33" can convert easily to number 33
 // "33abc" gives NaN coz it can't get converted
 // true = 1 false = 0
+
+/*to check boolean conversion*/ 
+// fot 1= true, 0=false, ""=false, "srushti"=true
+let IsLoggedIn = 1
+let BooleanisLoggedIn = Boolean(IsLoggedIn)
+console.log(BooleanisLoggedIn)
+
+/*for string conversion */
+let someno = 33
+let stringno = String(someno)
+console.log(stringno)
+console.log(typeof(stringno));

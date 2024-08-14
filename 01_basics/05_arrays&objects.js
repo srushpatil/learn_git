0a307bdce3f5efc0882aca318f2a893458bc9a9c
srushpatil <srushpatil231102@gmail.com>
2024-08-14 06:14:24 +0000

    arrays and object

---
+++ 01_basics/05_arrays&objects.js
@@ -0,0 +1,13 @@
+//arrays
+const heros = ["shaktiman", "naagraj", "doga"]
+
+//objects
+let myObj = {
+    name : "Srushti",
+    age : 22,
+}
+
+//function
+const myFunction = function(){
+    console.log("Hello World")
+}

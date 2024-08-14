643985bfd904ac90b2158bfa7f6dbf5d100f7eaa
srushpatil <srushpatil231102@gmail.com>
2024-08-14 04:56:41 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict" //treat all js code as new version
+
+//alert(3,3) //error occurs coz we are using nodejs not browser
+
+console.log(3+3)
+
+console.log("Srushti")
+
+let char = "Srushti"
+let age = 22
+let IsLoggedIn = false;
+
+//datatypes
+// number 
+// bigint
+// string = " "
+// boolean = true/false
+// null = stand alone value
+// undefined
+// symbol = used for uniqueness
+
+//to find the datatype
+console.log(typeof "srushti")
+console.log(typeof age);
+console.log(typeof null);  //the typeof null is always object
+console.log(typeof undefined);

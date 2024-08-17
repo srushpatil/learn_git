b9079a3548bafafe6a7da7289952db2982b7de8e
srushpatil <srushpatil231102@gmail.com>
2024-08-17 10:36:44 +0000

    Objects part 1

---
+++ 01_basics/10_Objects.js
@@ -0,0 +1,12 @@
+const JsUser = {
+    name: "Srushti",
+    age: 21,
+    location: "Kolhapur",
+    email: "srupatil@gmail.com",
+    isLoggedIn: false,
+    lastLoginDays: ["Monday", "Tuesday"]
+}
+
+console.log(JsUser.email);
+//or
+console.log(JsUser["email"])
